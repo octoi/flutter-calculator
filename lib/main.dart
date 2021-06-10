ecef560e3cc9a328cd6ffb495ffba568a210a6dd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-10 14:51:48 +0530

    added other items

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class CalculatorApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFF283637),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -39,7 +40,83 @@ class CalculatorApp extends StatelessWidget {
                   textColor: Color(0xFF65BDAC),
                 ),
               ],
-            )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalcButton(
+                  text: '7',
+                ),
+                CalcButton(
+                  text: '8',
+                ),
+                CalcButton(
+                  text: '9',
+                ),
+                CalcButton(
+                  text: '*',
+                  fillColor: Colors.white,
+                  textColor: Color(0xFF65BDAC),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalcButton(
+                  text: '4',
+                ),
+                CalcButton(
+                  text: '5',
+                ),
+                CalcButton(
+                  text: '6',
+                ),
+                CalcButton(
+                  text: '-',
+                  fillColor: Colors.white,
+                  textColor: Color(0xFF65BDAC),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalcButton(
+                  text: '1',
+                ),
+                CalcButton(
+                  text: '2',
+                ),
+                CalcButton(
+                  text: '3',
+                ),
+                CalcButton(
+                  text: '+',
+                  fillColor: Colors.white,
+                  textColor: Color(0xFF65BDAC),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalcButton(
+                  text: '.',
+                ),
+                CalcButton(
+                  text: '0',
+                ),
+                CalcButton(
+                  text: '00',
+                ),
+                CalcButton(
+                  text: '=',
+                  fillColor: Colors.white,
+                  textColor: Color(0xFF65BDAC),
+                ),
+              ],
+            ),
           ],
         ),
       ),
