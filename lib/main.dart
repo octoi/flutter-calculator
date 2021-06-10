5b8db7d1ded28526d288fbfc8086c91e6ae7d2c4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-10 14:41:58 +0530

    wrapped stuffs in column

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,32 @@ class CalculatorApp extends StatelessWidget {
       ),
       home: Scaffold(
         backgroundColor: Color(0xFF283637),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CalcButton(
-                text: 'AC',
-                fillColor: Color(0xFF6C807F),
-              ),
-              CalcButton(
-                text: 'C',
-                fillColor: Color(0xFF6C807F),
-              ),
-              CalcButton(
-                text: '%',
-                fillColor: Colors.white,
-                textColor: Color(0xFF65BDAC),
-              ),
-              CalcButton(
-                text: '/',
-                fillColor: Colors.white,
-                textColor: Color(0xFF65BDAC),
-              ),
-            ],
-          ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalcButton(
+                  text: 'AC',
+                  fillColor: Color(0xFF6C807F),
+                ),
+                CalcButton(
+                  text: 'C',
+                  fillColor: Color(0xFF6C807F),
+                ),
+                CalcButton(
+                  text: '%',
+                  fillColor: Colors.white,
+                  textColor: Color(0xFF65BDAC),
+                ),
+                CalcButton(
+                  text: '/',
+                  fillColor: Colors.white,
+                  textColor: Color(0xFF65BDAC),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
